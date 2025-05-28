a371e17ad281553da4719c48dc76bd9aa91c9bf3
arabella-a <aramdl.alegria02@gmail.com>
2025-05-28 01:03:07 -0700

    add footer in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Footer from "./Components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-
+      <Footer />
     </div>
   );
 }
