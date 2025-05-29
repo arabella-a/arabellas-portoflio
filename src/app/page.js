cb7d94b620d0a98cc44c10dedc6795008d76482c
arabella-a <aramdl.alegria02@gmail.com>
2025-05-29 11:22:49 -0700

    add recent works and footer in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Footer from "./Components/Footer/Footer";
+import RecentWorks from "./Components/RecentWorks/RecentWorks";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <main className={styles.main}>
+        <RecentWorks />
+      </main>
       <Footer />
     </div>
   );
